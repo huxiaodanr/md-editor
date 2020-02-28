12ea3564303af526b244bedecef13abc626291a9
huxiaodanr <851812859@qq.com>
2020-02-28 10:22:25 +0800

    feat: page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState<string>('简书编辑器模拟');
+  const [content, setContent] = useState<string>('');
+  const onTitltChange = (e: any) => {
+    setTitle(e.target.value);
+  };
+
+  const onContentChange = (e: any) => {
+    setContent(e.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app">
+        <div id="editor" className="editor">
+          <div className="editor-header">
+            <input
+              value={title}
+              className="header-input"
+              onChange={onTitltChange}
+              placeholder="请输入标题"
+            />
+          </div>
+          <div className="control-pannel">this is control pannel</div>
+          <div className="editor-content">
+            <textarea
+              className="content-textarea"
+              placeholder="请输入文章内容"
+              value={content}
+              onChange={onContentChange}
+            />
+          </div>
+        </div>
+        <div id="previewer" className="previewer">
+          <h1 className="previewer-header">{title}</h1>
+          <div className="previewer-content">{content}</div>
+        </div>
+      </div>
     </div>
   );
 }
