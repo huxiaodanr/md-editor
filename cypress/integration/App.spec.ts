1e4dff8c75b4df512a6e44837491233b948b2723
huxiaodanr <851812859@qq.com>
2020-03-09 22:17:02 +0800

    feat: add syntax test

--- cypress/integration/App.spec.ts
+++ cypress/integration/App.spec.ts
@@ -24,3 +24,45 @@ describe('The home page', () => {
     previewer().should('have.text', 'hello cypress');
   });
 });
+
+describe('Syntax', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('should render h1-h6 if rows start with #', () => {
+    editor()
+      .type('# 一级标题{enter}')
+      .type('## 二级标题{enter}')
+      .type('### 三级标题{enter}')
+      .type('#### 四级标题{enter}')
+      .type('##### 五级标题{enter}')
+      .type('###### 六级标题{enter}')
+      .type('####### 六级标题');
+    previewer().contains('h1', '一级标题');
+    previewer().contains('h2', '二级标题');
+    previewer().contains('h3', '三级标题');
+    previewer().contains('h4', '四级标题');
+    previewer().contains('h5', '五级标题');
+    previewer().contains('h6', '六级标题');
+  });
+  it('should render font style', () => {
+    editor()
+      .type('**加粗字体**')
+      .type('*倾斜字体*{enter}')
+      .type('***倾斜加粗字体***{enter}')
+      .type('~~删除线~~');
+    previewer().contains('b', '加粗字体');
+    previewer().contains('i', '倾斜字体');
+    previewer()
+      .get('i')
+      .contains('b', '倾斜加粗字体');
+    previewer().contains('s', '删除线');
+  });
+  it('should render image', () => {
+    editor().type(
+      '![image](https://www.baidu.com/img/bd_logo1.png?where=super)'
+    );
+    previewer().find('img');
+    previewer().should('not.contain.text', '![');
+  });
+});
